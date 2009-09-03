83bff2f5e3cbd69883de66f240913628f8908603
Gregory Van de Wiele <gregory@eai.be>
2009-09-03 15:47:12 +0200

    initial commit

---
+++ src/app/EAI.BE.BizTalk.PipelineComponents.CharacterTranscoder/TranscodingStream.cs
@@ -0,0 +1,225 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace EAI.BE.BizTalk.PipelineComponents
+{
+    /// <summary>
+    /// TranscodingStream class is a binary tranformation stream decorator. While reading the underlying bytestream is transcoded from a source to a target encoding, on the fly. There is no support for seeking nor writing.
+    /// </summary>
+    public class TranscodingStream : Stream
+    {
+        private Stream _stream;
+        private StreamReader _reader;
+        private Encoder _encoder;
+        private long _position = 0;
+
+        private Queue<byte> _buffer = new Queue<byte>(3);
+        private bool _bufferFilled = false;
+
+        private TranscodingStream(Stream stream, Encoding outEncoding) 
+        {
+            if (stream == null)
+                throw new ArgumentNullException("stream");
+
+            if (outEncoding == null)
+                throw new ArgumentNullException("outEncoding");
+
+            this._stream = stream;
+            this._encoder = outEncoding.GetEncoder();
+        }
+
+        public TranscodingStream(Stream stream, Encoding outEncoding, Encoding inEncoding) : this(stream, outEncoding)
+        {
+            if (inEncoding == null)
+                throw new ArgumentNullException("inEncoding");
+         
+            this._reader = new StreamReader(_stream, inEncoding);
+        }
+
+        public TranscodingStream(Stream stream, Encoding outEncoding, bool detectEncodingFromByteOrderMarks) : this(stream,outEncoding)
+        {
+            if (detectEncodingFromByteOrderMarks == false)
+            {
+                this._reader = new StreamReader(_stream, Encoding.UTF8);
+            }
+            else
+            {
+                this._reader = new StreamReader(_stream, true);
+            }
+
+        }
+
+        public override bool CanRead
+        {
+            get { return true; }
+        }
+
+        public override bool CanSeek
+        {
+            get { return false; }
+        }
+
+        public override bool CanWrite
+        {
+            get { return false; }
+        }
+
+        public override void Flush()
+        {
+            throw new Exception("The method or operation is not implemented.");
+        }
+
+        public override long Length
+        {
+            get 
+            {
+                throw new Exception("The method or operation is not implemented.");
+            }
+        }
+
+        public override long Position
+        {
+            get
+            {
+                return _position;
+            }
+            set
+            {
+                throw new Exception("The method or operation is not implemented.");
+            }
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            //if (_position < 0)
+            //    throw new InvalidOperationException();
+
+            int written = 0;
+            int open = offset + count > buffer.Length ? buffer.Length - offset : count;
+            int position = 0;
+            int read = 0;
+            int write = 0;
+            char[] readBuffer;
+
+            while (open > 0)
+            {
+                int maxSafeWrite = open / 4;
+                
+                if(maxSafeWrite>0)
+                {
+                    readBuffer = new char[maxSafeWrite];
+                    
+                    read = _reader.Read(readBuffer, 0, maxSafeWrite);
+                    
+                    if (read == 0)
+                        return written;
+
+                    write = _encoder.GetBytes(readBuffer, 0, read, buffer, offset + position, true);
+                    position += write;
+                    _position += write;
+                    written += write;
+                    
+                    if (read < maxSafeWrite)
+                        return written;
+                }
+                else
+                {
+                    readBuffer = new char[1];
+
+                    if (_bufferFilled == false)
+                    {
+                        read = _reader.Read(readBuffer, 0, 1);
+
+                        if (read == 0)
+                            return written;
+
+                        byte[] writeBuffer = new byte[_encoder.GetByteCount(readBuffer, 0, 1, false)];
+
+                        _encoder.GetBytes(readBuffer, 0, 1, writeBuffer, 0, true);
+
+                        if (writeBuffer.Length > open)
+                        {
+                            //more bytes are read from source than available in the output buffer 
+                            write = open;
+                            for (int i = 0; i < writeBuffer.Length - open; i++)
+                            {
+                                _buffer.Enqueue(writeBuffer[i + open]);
+                            }
+                            _bufferFilled = true;
+                        }
+                        else
+                        {
+                            write = writeBuffer.Length;
+                        }
+
+                        for (int i = 0; i < write; i++)
+                        {
+                            if (offset + position > buffer.GetUpperBound(0))
+                                return written;
+
+                            buffer[offset + position] = writeBuffer[i];
+                            position += 1;
+                            _position += 1;
+                            written += 1;
+
+                        }
+
+                    }
+                    else
+                    {
+                        
+                        if (_bufferFilled)
+                        {
+                            buffer[offset + position] = _buffer.Dequeue();
+                            _bufferFilled = _buffer.Count > 0;
+                            
+                            position += 1;
+                            _position += 1;
+                            written += 1;
+                            write = 1;
+                        }
+                        
+                    }
+                    
+                }
+
+                open = open - write;
+            }
+
+            return written;
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new Exception("The method or operation is not implemented.");
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new Exception("The method or operation is not implemented.");
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            throw new Exception("The method or operation is not implemented.");
+        }
+
+        // base close will call dispose below, so inner stream will effectively be close (streamreader closes stream inside dispose)
+        protected override void Dispose(bool disposing)
+        {
+            try
+            {
+                if (disposing)
+                {
+                    _reader.Dispose();
+                }
+            }
+            finally
+            {
+                base.Dispose(disposing);
+            }
+        }
+    }
+} 
---
+++ src/testing/EAI.BE.BizTalk.PipelineComponents.CharacterTranscoder.UnitTests/CharacterTranscoderTests.cs
@@ -0,0 +1,232 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using EAI.BE.BizTalk.PipelineComponents;
+using Microsoft.BizTalk.Message.Interop;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EAI.BE.BizTalk.PipelineComponents.CharacterTranscoder.UnitTests
+{
+    [TestClass]
+    public class CharacterTranscoderTests
+    {
+
+        [TestMethod]
+        public void MultiByteCharactersInSourceStreamDoNotGetLostStreaming()
+        {
+            string value = "Øþ";
+            byte[] bytesUTF8 = Encoding.UTF8.GetBytes(value);
+            byte[] expected = new byte[] { 0xD8, 0xFE };
+
+            Stream after = new TranscodingStream(new MemoryStream(bytesUTF8), Encoding.GetEncoding(1252), Encoding.UTF8);
+
+
+            Assert.IsTrue(ByteArrayCompare(StreamToArray(after), expected));
+        }
+
+        [TestMethod]
+        public void EmptyWrite()
+        {
+            string value = "";
+            byte[] bytesUTF8 = Encoding.UTF8.GetBytes(value);
+            byte[] expected = new byte[] { 0x41};
+            byte[] output = new byte[9];
+
+            Stream after = new TranscodingStream(new MemoryStream(bytesUTF8), Encoding.GetEncoding(1252), Encoding.UTF8);
+
+            int count = after.Read(output, 0, 1);
+
+            Assert.AreEqual(0, after.Read(output, 0, 1));
+        }
+
+        [TestMethod]
+        public void SourceStreamIsShorterThanDestinationStream()
+        {
+            string value = "A";
+            byte[] bytesUTF8 = Encoding.UTF8.GetBytes(value);
+            byte[] expected = new byte[100]; 
+            expected[0] = 0x41;
+            byte[] output = new byte[100];
+
+            Stream after = new TranscodingStream(new MemoryStream(bytesUTF8), Encoding.GetEncoding(1252), Encoding.UTF8);
+
+            int count = after.Read(output, 0, 100);
+
+            Assert.IsTrue(ByteArrayCompare(output, expected));
+        }
+
+
+        [TestMethod]
+        public void NoFullRead()
+        {
+            string value = "AAA";
+            byte[] bytesUTF8 = Encoding.UTF8.GetBytes(value);
+            byte[] expected = new byte[] { 0x41, 0x41};
+            byte[] output = new byte[2];
+
+            Stream after = new TranscodingStream(new MemoryStream(bytesUTF8), Encoding.UTF8, Encoding.UTF8);
+
+            int count = after.Read(output, 0, 2);
+
+            Assert.IsTrue(ByteArrayCompare(output, expected));
+        }
+
+        [TestMethod]
+        public void NoFullRead2()
+        {
+            string value = "BAA";
+            byte[] bytesUTF8 = Encoding.UTF8.GetBytes(value);
+            byte[] expected = new byte[] { 0x00, 0x42 };
+            byte[] output = new byte[2];
+
+            Stream after = new TranscodingStream(new MemoryStream(bytesUTF8), Encoding.UTF8, Encoding.UTF8);
+
+            int count = after.Read(output, 1, 1);
+
+            Assert.IsTrue(ByteArrayCompare(output, expected));
+        }
+
+        [TestMethod, ExpectedException(typeof(ObjectDisposedException))]
+        public void IdisposableTest()
+        {
+            string value = "BAA";
+            byte[] bytesUTF8 = Encoding.UTF8.GetBytes(value);
+
+            Stream inStream = new MemoryStream(bytesUTF8);
+
+
+            using (Stream after = new TranscodingStream(inStream, Encoding.UTF8, Encoding.UTF8))
+            {
+                while (after.ReadByte() != -1)
+                { }
+            }
+
+            long length = inStream.Length;
+        }
+
+        [TestMethod, ExpectedException(typeof(ObjectDisposedException))]
+        public void IdisposableTest2()
+        {
+            string value = "BAA";
+            byte[] bytesUTF8 = Encoding.UTF8.GetBytes(value);
+
+            Stream inStream = new MemoryStream(bytesUTF8);
+
+            Stream after = new TranscodingStream(inStream, Encoding.UTF8, Encoding.UTF8);
+            after.Close();
+
+            long length = inStream.Length;
+        }
+
+        [TestMethod]
+        public void Decode_UTF32_And_Encode_1252()
+        {
+            string value = "AØþ";
+            byte[] bytesUTF32 = Encoding.UTF32.GetBytes(value);
+            byte[] bytes1252 = Encoding.GetEncoding(1252).GetBytes(value);
+            byte[] output = new byte[bytes1252.Length];
+
+            Stream after = new TranscodingStream(new MemoryStream(bytesUTF32), Encoding.GetEncoding(1252), Encoding.UTF32);
+
+            Assert.IsTrue(ByteArrayCompare(StreamToArray(after), bytes1252));
+        }
+
+        [TestMethod]
+        public void Decode_1252_And_Encode_UTF32()
+        {
+            string value = "AØþ";
+            byte[] bytesUTF32 = Encoding.UTF32.GetBytes(value);
+            byte[] bytes1252 = Encoding.GetEncoding(1252).GetBytes(value);
+            byte[] output = new byte[bytesUTF32.Length];
+
+            Stream after = new TranscodingStream(new MemoryStream(bytes1252), Encoding.UTF32, Encoding.GetEncoding(1252));
+
+            Assert.IsTrue(ByteArrayCompare(StreamToArray(after), bytesUTF32));
+        }
+
+
+        [TestMethod]
+        public void Decode_UTF8_And_Encode_UTF16()
+        {
+            string value = "ABCDEFGHØþ";
+            byte[] bytesUTF8 = Encoding.UTF8.GetBytes(value);
+            byte[] bytesUTF16 = Encoding.Unicode.GetBytes(value);
+            byte[] output = new byte[bytesUTF16.Length];
+
+            Stream after = new TranscodingStream(new MemoryStream(bytesUTF8), Encoding.Unicode, Encoding.UTF8);
+            int count = after.Read(output, 0, 40);
+
+            Assert.IsTrue(ByteArrayCompare(output, bytesUTF16));
+        }
+
+        [TestMethod]
+        public void Decode_And_Encode_Same_Encoding_UTF16()
+        {
+            string value = "ABCDEFGHØþ";
+            byte[] bytesUTF16 = Encoding.Unicode.GetBytes(value);
+            byte[] output = new byte[bytesUTF16.Length];
+
+            Stream after = new TranscodingStream(new MemoryStream(bytesUTF16), Encoding.Unicode, Encoding.Unicode);
+            int count = after.Read(output, 0, 40);
+
+            Assert.IsTrue(ByteArrayCompare(output, bytesUTF16));
+        }
+
+        [TestMethod]
+        public void Decode_And_Encode_Same_Encoding_UTF8()
+        {
+            string value = "ABCDEFGHØþ";
+            byte[] bytesUTF8 = Encoding.UTF8.GetBytes(value);
+            byte[] output = new byte[bytesUTF8.Length];
+
+            Stream after = new TranscodingStream(new MemoryStream(bytesUTF8), Encoding.UTF8, Encoding.UTF8);
+            int count = after.Read(output, 0, 40);
+
+            Assert.IsTrue(ByteArrayCompare(output, bytesUTF8));
+        }
+
+        static bool ByteArrayCompare(byte[] a1, byte[] a2)
+        {
+            if (a1.Length != a2.Length)
+                return false;
+            for (int i = 0; i < a1.Length; i++)
+                if (a1[i] != a2[i])
+                    return false;
+            return true;
+        }
+
+        public static MemoryStream Fix3F(Stream source)
+        {
+            MemoryStream target = new MemoryStream();
+            Encoding targetEncoding = Encoding.GetEncoding(1252);
+
+            StreamReader sourceReader = new StreamReader(source, Encoding.UTF8, false);
+            StreamWriter targetWriter = new StreamWriter(target, targetEncoding);
+
+            int charRead = sourceReader.Read();
+            while (charRead != -1)
+            {
+                targetWriter.Write((char)charRead);
+                charRead = sourceReader.Read();
+            }
+            targetWriter.Flush();
+            target.Seek(0, SeekOrigin.Begin);
+            return target;
+        }
+
+        private static byte[] StreamToArray(Stream bytes)
+        {
+            int read = bytes.ReadByte();
+            List<byte> listOfBytes = new List<byte>();
+            while (read != -1)
+            {
+                listOfBytes.Add((byte)read);
+                read = bytes.ReadByte();
+            }
+            byte[] output = listOfBytes.ToArray();
+            
+            return output;
+        }
+    }
+}
