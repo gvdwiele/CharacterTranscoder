4f53715b836d9d0ab554fec4a3f13c2afef96c19
Gregory Van de Wiele <info@eai.be>
2009-09-15 16:09:09 +0200

    Fixed not working .snk references

--- src/app/EAI.BE.BizTalk.PipelineComponents.CharacterTranscoder.Schemas/AssemblyInfo.cs
+++ src/app/EAI.BE.BizTalk.PipelineComponents.CharacterTranscoder.Schemas/AssemblyInfo.cs
@@ -18,7 +18,8 @@
 [assembly: System.Reflection.AssemblyCulture("")]
 [assembly: System.Reflection.AssemblyVersion("1.0.0.0")]
 [assembly: System.Reflection.AssemblyDelaySign(false)]
-[assembly: System.Reflection.AssemblyKeyFile("")]
+[assembly: System.Reflection.AssemblyKeyFile("..\\..\\..\\EAI.BE.BizTalk.PipelineComponents.CharacterTranscoder\\CharacterTranscode" +
+    "r.snk")]
 [assembly: System.Reflection.AssemblyKeyName("")]
 [assembly: System.Reflection.AssemblyDefaultAlias("")]
 [assembly: System.Reflection.AssemblyFileVersion("1.0.0.0")]
