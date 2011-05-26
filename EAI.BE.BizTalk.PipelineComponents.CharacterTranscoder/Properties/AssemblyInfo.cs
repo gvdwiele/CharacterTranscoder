bfe2353148742531d3167cd0991a8a20e51169ab
Gregory Van de Wiele <gregory@eai.be>
2011-05-26 10:39:32 +0200

    Complete build script update

--- EAI.BE.BizTalk.PipelineComponents.CharacterTranscoder/Properties/AssemblyInfo.cs
+++ EAI.BE.BizTalk.PipelineComponents.CharacterTranscoder/Properties/AssemblyInfo.cs
@@ -22,14 +22,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("28ff0f95-2b27-46ec-b170-bcb06e9e9278")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
