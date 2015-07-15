d49b1afb5b34a29e93921536084aa5289bb43819
gregory van de wiele <gregory@eai.be>
2015-07-15 14:07:41 +0200

    Backup: remove

--- Backup/EAI.BE.BizTalk.PipelineComponents.CharacterTranscoder.TestArtifacts/Properties/CommonAssemblyInfo.cs
+++
@@ -1,3 +0,0 @@
-[assembly: System.Reflection.AssemblyVersion("1.0.0.0")]
-[assembly: System.Reflection.AssemblyDelaySign(false)]
-[assembly: System.Reflection.AssemblyKeyFile("CharacterTranscoder.snk")]
